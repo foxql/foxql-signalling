fbcb1b3526e48266d052691ddfb2a89e70c6557a
boraozer <nagayapim@gmail.com>
2021-05-16 21:34:44 +0300

    deprecated listeners deleted

--- src/events/actionList.js
+++
@@ -1,6 +0,0 @@
-const name = 'actionList';
-
-exports.listener = (socket, server, data) => {
-    socket.emit(name, server.actions.list())
-}
-exports.name = name;
--- src/events/getTrends.js
+++
@@ -1,8 +0,0 @@
-const name = 'getTrends';
-
-exports.listener = (socket, server, country) => {
-    socket.emit(name, 
-        server.trends.list(country)    
-    )
-}
-exports.name = name;
