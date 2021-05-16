b141af4ce7f38b219149fa33ed5aa88101d2745e
boraozer <nagayapim@gmail.com>
2021-05-16 21:34:32 +0300

    deleted deprecated listeners & method. Added dotenv config

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
+require('dotenv').config()
+
 const foxql = require('@foxql/foxql-server');
-const ActionMap = require('./src/core/actionMap.js')
 
 let server = new foxql.server(connection);
-
-server.actions = new ActionMap();
-
 
 server.use('serverOptions', {
     port : process.env.PORT || 3000,
     host : '0.0.0.0'
 });
-
-server.pushEvent(
-    require('./src/events/newDoc.js')
-);
-
-server.pushEvent(
-    require('./src/events/getTrends.js')
-);
-
-server.pushEvent(
-    require('./src/events/actionList.js')
-)
-
-server.pushEvent(
-    require('./src/events/search.js')
-)
-
 
 server.open();
 
@@ -35,12 +16,7 @@ async function connection(socket)
 {
     console.log(socket.id, 'Bağlandı.');
 
-    /*server.actions.new({
-        type : 'new-connection',
-        socketId : socket.id
-    });*/
-
-    socket.broadcast.emit('actionList', server.actions.list())
+    /** Your custom socket listeners... */
 
     
 }
